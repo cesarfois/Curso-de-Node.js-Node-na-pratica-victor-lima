601dd47556e5f0f0bf02f5965d9519b3f2eb4500
unknown <fois2010@gmail.com>
2023-01-05 18:18:17 -0300

    aula3 -basico

---
+++ Aula3/helloworld.js
@@ -0,0 +1,5 @@
+var nome = "Cesar"
+var sobrenome = "Fois"
+
+
+console.log("bienvenido ",nome + sobrenome)
---
+++ Aula3/somar.js
@@ -0,0 +1,8 @@
+var num1 = 90
+var num2 = 10
+
+const n1n2 = (a,b) =>  a + b
+
+
+
+console.log(n1n2(20, 2))
